448ed3236bdf0ac806df9e2c33615c1428c3b69c
SayedXAn <106014255+SayedXAn@users.noreply.github.com>
2025-11-30 16:44:34 +0600

    Signs floating adjusted

--- Signature Wall - Refined/Assets/Scripts/FloatingObject.cs
+++ Signature Wall - Refined/Assets/Scripts/FloatingObject.cs
@@ -9,8 +9,10 @@ public class FloatingObject : MonoBehaviour
         // pop up entry animation and on finish, pop out exit animation
         transform.DOScale(Vector3.zero, 1f).From().SetEase(Ease.OutBounce).OnComplete(() =>
         {
-            float randomY= transform.position.y + Random.Range(-1, 1f);
-            float randomX = transform.position.x + Random.Range(-2f, -2f);
+            //float randomY= transform.position.y + Random.Range(-1, 1f);
+            //float randomX = transform.position.x + Random.Range(-2f, -2f);
+            float randomY = transform.position.y + Random.Range(-9f, 9f);
+            float randomX = transform.position.x + Random.Range(-3f, 3f);
             transform.DOLocalMove(new Vector3(randomX, randomY, 0), Random.Range(5, 15)).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
             transform.DOLocalRotate(new Vector3(0, 0, Random.Range(-90, 90)), Random.Range(10, 20))
             .SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
